fe4c29b3dd43acd5047d391061f61ee04e935c6b
Eitan Adler <lists@eitanadler.com>
2020-02-22 11:14:05 -0800

    list: first: implement push

--- too-many-ll/lists/src/first.rs
+++ too-many-ll/lists/src/first.rs
@@ -1,5 +1,32 @@
+use std::mem;
+
 #[derive(Debug)]
-pub enum List {
+pub struct List {
+    head: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty)
+        });
+        self.head = Link::More(new_node);
+    }
+}
+
+#[derive(Debug)]
+enum Link {
     Empty,
-    Elem(i32, Box<List>),
+    More(Box<Node>),
+}
+
+#[derive(Debug)]
+struct Node {
+    elem: i32,
+    next: Link,
 }
