0ab1dec41907f479ccc18542c89d9b99b2742174
Eitan Adler <lists@eitanadler.com>
2020-02-22 18:46:42 -0800

    list: second: generics! & peek

--- too-many-ll/lists/src/second.rs
+++ too-many-ll/lists/src/second.rs
@@ -1,66 +1,65 @@
-use std::mem;
-
 #[derive(Debug)]
-pub struct List {
-    head: Option<Box<Node>>,
+pub struct List<T> {
+    head: Option<Box<Node<T>>>,
 }
 
-impl List {
+impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
     }
 
-    pub fn push(&mut self, elem: i32) {
-        let new_node: Box<Node> = Box::new(Node {
+    pub fn push(&mut self, elem: T) {
+        let new_node: Box<Node<T>> = Box::new(Node {
             elem: elem,
             next: self.head.take()
         });
         self.head = Some(new_node);
     }
 
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             self.head = node.next;
             node.elem
         })
     }
+
+    pub fn peek(&mut self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+          &node.elem
+        })
+    }
 }
 
-impl Drop for List {
+impl<T> Drop for List<T> {
     fn drop(&mut self) {
-        let mut cur_link: Option<Box<Node>> = self.head.take();
+        let mut cur_link: Option<Box<Node<T>>> = self.head.take();
         while let Some(mut boxed_node) = cur_link {
-            cur_link = mem::replace(&mut boxed_node.next, None);
+            cur_link = boxed_node.next.take();
         }
     }
 }
 
 
 #[derive(Debug)]
-struct Node {
-    elem: i32,
-    next: Option<Box<Node>>,
+struct Node<T> {
+    elem: T,
+    next: Option<Box<Node<T>>>,
 }
 
 #[cfg(test)]
 mod test {
-    use crate::first::List;
-
-    #[test]
-    fn can_be_constructed() {
-        let _ = List::new;
-    }
+    use crate::second::List;
 
     #[test]
     fn push_value_pops_value() {
-        let mut list = List::new();
+        let mut list: List<i32> = List::new();
         list.push(1);
         assert_eq!(list.pop(), Some(1));
     }
 
     #[test]
     fn push_pop_multiple_values() {
-        let mut list = List::new();
+        let mut list: List<i32> = List::new();
         list.push(1);
         list.push(2);
         list.push(3);
@@ -71,7 +70,7 @@ mod test {
 
     #[test]
     fn push_pop_and_repushes() {
-        let mut list = List::new();
+        let mut list: List<i32> = List::new();
         list.push(1);
         assert_eq!(list.pop(), Some(1));
         list.push(2);
@@ -81,14 +80,23 @@ mod test {
 
     #[test]
     fn no_push_gets_none() {
-        let mut list = List::new();
+        let mut list: List<i32> = List::new();
         assert_eq!(list.pop(), None)
     }
 
     #[test]
     fn push_pop_gets_value_and_none_after() {
-        let mut list = List::new();
+        let mut list: List<i32> = List::new();
         list.push(1);
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None)
+    }
+
+    #[test]
+    fn test_push_and_peek_and_pops_then_none() {
+        let mut list: List<i32> = List::new();
+        list.push(1);
+        assert_eq!(list.peek(), Some(&1));
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None)
     }
