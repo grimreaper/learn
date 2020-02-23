4e5207c63b3f616df9062d103235854ba3dc15c8
Eitan Adler <lists@eitanadler.com>
2020-02-22 21:11:17 -0800

    list: second: add next iter

--- too-many-ll/lists/src/second.rs
+++ too-many-ll/lists/src/second.rs
@@ -1,9 +1,16 @@
+use std::slice::IterMut;
+
 #[derive(Debug)]
 pub struct List<T> {
     head: Option<Box<Node<T>>>,
 }
 
 pub struct IntoIter<T>(List<T>);
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
 
 impl<T> List<T> {
     pub fn new() -> Self {
@@ -40,12 +47,28 @@ impl<T> List<T> {
     pub fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
     }
+
+    pub fn iter<'a>(&'a self)  -> Iter<'a, T> {
+        Iter {
+            next: self.head.as_ref().map(|node| &**node)
+        }
+    }
 }
 
 impl<T> Iterator for IntoIter<T> {
     type Item = T;
     fn next(&mut self) -> Option<Self::Item> {
         self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.elem
+        })
     }
 }
 
@@ -142,6 +165,18 @@ mod test {
         let mut iter = list.into_iter();
         assert_eq!(iter.next(), Some(2));
         assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter_pops_and_ends_in_none() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
         assert_eq!(iter.next(), None);
     }
 }
