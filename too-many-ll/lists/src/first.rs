d817bc048a20ed74418159edd878469af148bc95
Eitan Adler <lists@eitanadler.com>
2020-02-22 18:35:47 -0800

    list: first: add drop

--- too-many-ll/lists/src/first.rs
+++ too-many-ll/lists/src/first.rs
@@ -31,6 +31,16 @@ impl List {
     }
 }
 
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link: Link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
+}
+
+
 #[derive(Debug)]
 enum Link {
     Empty,
